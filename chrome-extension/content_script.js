df3ac6b584b0c6fd8f66f102a373da553edf1a48
= <zeravla.miguel@gmail.com>
2026-03-06 12:46:31 -0600

    creacion de content_script

--- chrome-extension/content_script.js
+++ chrome-extension/content_script.js
@@ -1,19 +1,23 @@
 document.addEventListener('visibilitychange', () => {
     if (document.visibilityState === 'hidden') {
+        console.log('Cambio de pestaña o aplicación detectada.');
         enviarAlerta('Cambio de pestaña o aplicación detectado.');
     }
 });
 
 document.addEventListener('blur', () => {
+    console.log('El usaurio salió de la ventana del examen.');
     enviarAlerta('El usuario salió de la ventana del examen.');
 });
 
 document.addEventListener('copy', () => {
+    console.log('Intento de copiar contenido detectado.');
     enviarAlerta('Intento de copiar contenido detectado.');
 });
 
 document.addEventListener('contextmenu', (e) => {
     e.preventDefault();
+    console.log('Intento de abrir menú contextual (clic derecho)');
     enviarAlerta('Intento de abrir menu contextual (clic derecho)');
 });
 
